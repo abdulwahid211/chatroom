21bb1558fdbca32f1c502300d29956efed2fce1f
Abdul Wahid <abdul.wahid@machinemax.com>
2019-12-11 15:21:13 +0000

    mysql setup

--- flask/app/yoo.py
+++ flask/app/yoo.py
@@ -1,8 +1,28 @@
 from app import app
-from flask import jsonify 
+from flask import jsonify, request
+from flask_mysqldb import MySQL
 
-@app.route("/apple")
+
+app.config['MYSQL_HOST'] = 'docker-local-mysql'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_DB'] = 'gymbuddy'
+
+mysql = MySQL(app)
+
+@app.route("/apple", methods=['POST'])
 def apple():
+    if request.method == "POST":
+        firstName = request.form['fname']
+        lastName = request.form['lname']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
+        mysql.connection.commit()
+        cur.close()
+        return jsonify(fname=firstName,lastName=lastName)
+    
+    
     return jsonify(username="Abzy211")
 
 
---
+++ readme.md
@@ -0,0 +1,1 @@
+docker exec -it mysql1 mysqld -u root -p
