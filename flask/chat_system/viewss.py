f8ab4781c0bdb0a8cee8e488c4bc794149b28c05
Abdul Wahid <abdul.wahid@machinemax.com>
2020-02-24 23:01:02 +0000

    added db

--- flask/chat_system/viewss.py
+++ flask/chat_system/viewss.py
@@ -1,6 +1,6 @@
 from chat_system import app
 from chat_system import socketio
-from flask import render_template
+from flask import jsonify, render_template
 from data_config import mysql
 import json
 
@@ -11,16 +11,36 @@ def sessions():
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
 
+
+@socketio.on('user_connect')
+def broadcast_to_all_new_users(response, methods=['GET','POST']):
+    response = list()
+    cur = mysql.connection.cursor()
+    result = ''
+    try:
+        cur.execute("select * from messages;")
+        result = cur.fetchall()  # fetches all the rows in a result set
+    except:
+        print('Error:Unable to fetch data.')
+    cur.close()
+
+    for x, y in result:
+        print(str(x) +':'+ str(y))
+        op = {"user_name":x,"message":y}
+        response.append(op)
+    response = json.dumps(response)
+    print(response)
+    socketio.emit('broadcast', response, callback=messageReceived)
+
+
 @socketio.on('my event')
 def handle_my_custom_event(response, methods=['GET', 'POST']):
     print('received my event: ' + str(response))
     print(response)
 
-    format = json.dumps(response)
-    data = json.loads(format)
     try:
-        _username = data["user_name"]
-        _message = data["message"]
+        _username = response["user_name"]
+        _message = response["message"]
         cur = mysql.connection.cursor()
         cur.execute("INSERT INTO messages(name, message) VALUES (%s, %s)", (_username, _message))
         mysql.connection.commit()
@@ -28,3 +48,5 @@ def handle_my_custom_event(response, methods=['GET', 'POST']):
         print("KeyError")
 
     socketio.emit('my response', response, callback=messageReceived)
+
+
